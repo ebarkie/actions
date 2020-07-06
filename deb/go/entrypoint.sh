c950b301048e13a4ba5390d138ec53281285685f
Eric Barkie <ebarkie@users.noreply.github.com>
2020-07-05 21:38:55 -0400

    Improve versioning in deb/go

--- deb/go/entrypoint.sh
+++ deb/go/entrypoint.sh
@@ -4,10 +4,11 @@ set -euo pipefail
 
 env | grep -e "^INPUT"
 
+debchange -v ${INPUT_VERSION#"v"} -D stable -m "Release ${INPUT_VERSION}"
 export GOPROXY="https://goproxy.io,direct"
-dpkg-buildpackage --host-arch $INPUT_HOSTARCH -uc -us -b -d
+dpkg-buildpackage --host-arch ${INPUT_HOSTARCH} -uc -us -b -d
 
-asset_name="$(basename $GITHUB_REPOSITORY)_0.1-1_$INPUT_HOSTARCH.deb"
+asset_name=$(basename $(ls ../station-uploader_*_${INPUT_HOSTARCH}.deb))
 echo "::set-output name=asset_name::$asset_name"
 echo "::set-output name=asset_path::../$asset_name"
 
