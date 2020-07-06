8f14f24b29d20b78ab80de7fe18dd3569b0c1125
Eric Barkie <ebarkie@users.noreply.github.com>
2020-07-05 22:43:32 -0400

    Try to fix asset path issue

--- deb/go/entrypoint.sh
+++ deb/go/entrypoint.sh
@@ -9,8 +9,10 @@ debchange -v ${version} -D stable -m "Release v${version}"
 export GOPROXY="https://goproxy.io,direct"
 dpkg-buildpackage --host-arch ${INPUT_HOSTARCH} -uc -us -b -d
 
-asset_name=$(basename $(ls ../station-uploader_*_${INPUT_HOSTARCH}.deb))
-echo "::set-output name=asset_name::$asset_name"
-echo "::set-output name=asset_path::../$asset_name"
+mkdir build
+mv -v ../*.deb build
+asset_path=$(ls build/*.deb)
+echo "::set-output name=asset_name::${asset_path##*/}"
+echo "::set-output name=asset_path::${asset_path}"
 
 exit 0
